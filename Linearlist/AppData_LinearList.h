8e47ec4ae6af5af74d4a51d679299bde5dfda6f6
VitozzzZ <zealucius@gmail.com>
2017-10-12 21:27:58 -0500

    Create AppData_LinearList.h

---
+++ Linearlist/AppData_LinearList.h
@@ -0,0 +1,10 @@
+//实例数据元素类型定义
+#define STUDENT ElementType     //实例数据元素句柄化
+class STUDENT{                  //实例数据元素类型的定义
+public:
+    char number[10];
+    char name[8];
+    char sex[3];
+    int age;
+    char place[20];
+};
